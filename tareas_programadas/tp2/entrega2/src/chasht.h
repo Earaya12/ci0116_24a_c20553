e5a516443aff100476d785996e4483bd23c2a4eb
Ericka Araya <ericka.arayahidalgo@ucr.ac.cr>
2024-06-24 18:59:19 -0600

    Doxygen, metodo estudiante, linter de tabla hash

--- tareas_programadas/tp2/entrega2/src/chasht.h
+++ tareas_programadas/tp2/entrega2/src/chasht.h
@@ -1,3 +1,4 @@
+// Copyright 2024 Ericka Araya Hidalgo
 //
 //  chtable.h
 //
@@ -5,46 +6,79 @@
 #ifndef chtable_h
 #define chtable_h
 
-#include "llist.h"
 #include <string>
 #include <vector>
 
+#include "llist.h"
 
+/**
+ * @class chtable
+ * @brief Clase para representar una tabla hash encadenada.
+ * @tparam T Tipo de dato almacenado en la tabla.
+ */
 template <typename T>
 class chtable {
-public:
-    // Constructor que especifica el tamaño de la tabla
-    chtable(int sz) : size(sz), table(sz) {}
+ public:
+    /**
+     * @brief Constructor que especifica el tamaño de la tabla.
+     * @param sz Tamaño de la tabla hash.
+     */
+    explicit chtable(int sz) : size(sz), table(sz) {}
 
-    // Destructor de la tabla hash
+    /**
+     * @brief Destructor de la tabla hash.
+     */
     ~chtable() {}
 
-    // Inserta el elemento en la tabla
+    /**
+     * @brief Inserta un elemento en la tabla hash.
+     * @param k Elemento a insertar.
+     */
     void Insert(const T& k) {
         int index = HashFunction(k);
         llnode<T>* newNode = new llnode<T>(k);
         table[index].Insert(newNode);
     }
 
-    // Retorna un puntero a la llave o nullptr si no se encuentra
+    /**
+     * @brief Busca un elemento en la tabla hash.
+     * @param k Elemento a buscar.
+     * @return Puntero al elemento encontrado o nullptr si no se encuentra.
+     */
     T* Search(const T& k) {
         int index = HashFunction(k);
         llnode<T>* foundNode = table[index].Search(k);
-        if (foundNode != table[index].Search(0)) {
+        if (foundNode != nullptr) {
             return &foundNode->getKey();
         } else {
             return nullptr;
         }
     }
 
-private:
-    // Número de entradas en la tabla
-    int size;
-    
-    // La tabla es un vector de listas doblemente enlazadas
+    /**
+     * @brief Elimina un elemento de la tabla hash.
+     * @param k Elemento a eliminar.
+     */
+    void Delete(const T& k) {
+        int index = HashFunction(k);
+        table[index].Delete(k);
+    }
+
+    /// @brief Retorna informacion de estudiante
+    std::string ImprimirDatosDeTarea() {
+        return "c20553 Tarea 2 Etapa 2";
+    }
+
+ private:
+    int size;  ///< Número de entradas en la tabla.
+    ///< La tabla es un vector de listas doblemente enlazadas.
     std::vector<llist<T>> table;
 
-    // Función de dispersión
+    /**
+     * @brief Función de dispersión.
+     * @param k Elemento del cual se calculará el índice.
+     * @return Índice calculado a partir del elemento.
+     */
     int HashFunction(const T& k) const {
         return k % size;
     }
--- tareas_programadas/tp2/entrega2/src/llist.h
+++ tareas_programadas/tp2/entrega2/src/llist.h
@@ -1,3 +1,4 @@
+// Copyright 2024 Ericka Araya Hidalgo
 //
 //  llist.h
 //
@@ -11,12 +12,12 @@
 /// @tparam T Tipo de dato de la llave almacenada en el nodo.
 template <typename T>
 class llnode {
-private:
-    T key; ///< Llave del nodo.
-    llnode<T>* prev; ///< Puntero al nodo anterior.
-    llnode<T>* next; ///< Puntero al nodo siguiente.
+ private:
+    T key;  ///< Llave del nodo.
+    llnode<T>* prev;  ///< Puntero al nodo anterior.
+    llnode<T>* next;  ///< Puntero al nodo siguiente.
 
-public:
+ public:
     /// @brief Constructor por omisión.
     llnode() : key(T()), prev(nullptr), next(nullptr) {}
 
@@ -24,7 +25,8 @@ public:
     /// @param k Llave del nodo.
     /// @param w Puntero al nodo anterior.
     /// @param y Puntero al nodo siguiente.
-    llnode(const T& k, llnode<T>* w = nullptr, llnode<T>* y = nullptr) : key(k), prev(w), next(y) {}
+    llnode(const T& k, llnode<T>* w = nullptr,
+                     llnode<T>* y = nullptr) : key(k), prev(w), next(y) {}
 
     /// @brief Destructor.
     ~llnode() {}
@@ -70,61 +72,78 @@ public:
 /// @tparam T Tipo de dato de las llaves almacenadas en la lista.
 template <typename T>
 class llist {
-private:
-    llnode<T>* nil; ///< Nodo centinela.
+ private:
+    llnode<T>* nil;  ///< Nodo centinela.
 
+
+ public:
     /// @brief Retorna informacion de estudiante
     std::string ImprimirDatosDeTarea() {
         return "c20553 Tarea 2 Etapa 1";
     }
 
-public:
     /// @brief Constructor por omisión (crea una lista vacía).
     llist() {
-        this->nil = new llnode<T>(); // Crea el nodo centinela.
-        this->nil->setNext(this->nil); // Inicializa el nodo centinela apuntándose a sí mismo.
-        this->nil->setPrev(this->nil); // Inicializa el nodo centinela apuntándose a sí mismo.
+        this->nil = new llnode<T>();  // Crea el nodo centinela.
+        // Inicializa el nodo centinela apuntándose a sí mismo.
+        this->nil->setNext(this->nil);
+        this->nil->setPrev(this->nil);
     }
 
     /// @brief Destructor (borra la lista).
     ~llist() {
-        llnode<T>* current = this->nil->getNext(); // Empieza desde el primer nodo.
-        while (current != this->nil) { // Recorre la lista hasta llegar al nodo centinela.
-            llnode<T>* temp = current; // Guarda el nodo actual.
-            current = current->getNext(); // Avanza al siguiente nodo.
-            delete temp; // Elimina el nodo guardado.
+        // Empieza desde el primer nodo.
+        llnode<T>* current = this->nil->getNext();
+        // Recorre la lista hasta llegar al nodo centinela.
+        while (current != this->nil) {
+            llnode<T>* temp = current;  // Guarda el nodo actual.
+            current = current->getNext();  // Avanza al siguiente nodo.
+            delete temp;  // Elimina el nodo guardado.
         }
-        delete this->nil; // Elimina el nodo centinela.
+        delete this->nil;  // Elimina el nodo centinela.
     }
 
     /// @brief Inserta el nodo x en la lista.
     /// @param x Puntero al nodo a insertar.
     void Insert(llnode<T>* x) {
-        x->setNext(this->nil->getNext()); // Establece el siguiente nodo de x como el primer nodo actual.
-        this->nil->getNext()->setPrev(x); // Establece x como el nodo anterior del primer nodo actual.
-        x->setPrev(this->nil); // Establece el nodo centinela como el nodo anterior de x.
-        this->nil->setNext(x); // Establece x como el primer nodo después del nodo centinela.
+        // Establece el siguiente nodo de x como el primer nodo actual.
+        x->setNext(this->nil->getNext());
+        // Establece x como el nodo anterior del primer nodo actual.
+        this->nil->getNext()->setPrev(x);
+        // Establece el nodo centinela como el nodo anterior de x.
+        x->setPrev(this->nil);
+        // Establece x como el primer nodo después del nodo centinela.
+        this->nil->setNext(x);
     }
 
     /// @brief Busca la llave k iterativamente en la lista.
     /// @param k Llave a buscar.
-    /// @return Puntero al nodo que contiene la llave, o el nodo centinela si no se encuentra.
+    /// @return Puntero al nodo que contiene la llave, o el nodo centinela si no
+    /// se encuentra.
     llnode<T>* Search(const T& k) {
-        this->nil->setKey(k); // Usa el nodo centinela para simplificar la lógica de búsqueda.
-        llnode<T>* x = this->nil->getNext(); // Empieza desde el primer nodo.
-        while (x->getKey() != k) { // Recorre la lista hasta encontrar la llave o llegar al nodo centinela.
-            x = x->getNext(); // Avanza al siguiente nodo.
+        // Usa el nodo centinela para simplificar la lógica de búsqueda.
+        this->nil->setKey(k);
+        llnode<T>* x = this->nil->getNext();  // Empieza desde el primer nodo.
+        // Recorre la lista hasta encontrar la llave o llegar al nodo centinela.
+        while (x->getKey() != k) {
+            x = x->getNext();  // Avanza al siguiente nodo.
         }
-        return (x == this->nil) ? this->nil : x; // Devuelve el nodo encontrado o el nodo centinela.
+        // Devuelve el nodo encontrado o el nodo centinela.
+        return (x == this->nil) ? this->nil : x;
     }
 
     /// @brief Elimina la llave contenida en el nodo apuntado por x.
     /// @param x Puntero al nodo a eliminar.
     void Delete(llnode<T>* x) {
-        if (x != this->nil) { // Asegura que el nodo a eliminar no sea el nodo centinela.
-            x->getPrev()->setNext(x->getNext()); // Establece el siguiente nodo del anterior como el siguiente del nodo a eliminar.
-            x->getNext()->setPrev(x->getPrev()); // Establece el nodo anterior del siguiente como el anterior del nodo a eliminar.
-            delete x; // Elimina el nodo.
+        // Asegura que el nodo a eliminar no sea el nodo centinela.
+        if (x != this->nil) {
+            // Establece el siguiente nodo del anterior como el siguiente del
+            // nodo a eliminar.
+            x->getPrev()->setNext(x->getNext());
+            // Establece el nodo anterior del siguiente como el anterior del
+            // nodo a eliminar.
+            x->getNext()->setPrev(x->getPrev());
+            delete x;  // Elimina el nodo.
         }
     }
 };
